47d0fcb9b227e51879458f0bf7e2f1284e760a45
Seongjun Kim <isair.zun@gmail.com>
2014-12-10 01:52:45 +0900

    Infinate Scroll in MangaList

--- src/js/components/MangaList.js
+++ src/js/components/MangaList.js
@@ -10,37 +10,56 @@ var MangaNode = React.createClass({
 
     return (
       <li className="manga">
-        <a href={link} onClick={this._load}>
-          <img src={imgSrc} />
+        <a className={this.props.hidden ? 'hidden_elem' : ''} ref='link' href={link} onClick={this._onClick}>
+          <div className="thumbnail" style={{backgroundImage:'url('+imgSrc+')'}} />
           <h3 className="title">{this.props.data.title}</h3>
         </a>
       </li>
     );
   },
 
-  _load: function () {
-    var url = this.getDOMNode().children[0].href;
+  _onClick: function (e) {
+    var url = this.refs.link.props.href;
+    console.log(this.refs.link);
     this.props.load(url);
+    e.stopPropagation();
     return false;
   }
 });
 
 var MangaList = React.createClass({
+  getInitialState: function () {
+    return {top: 0, bottom: 0};
+  },
+
+  componentDidMount: function () {
+    this._onScroll();
+  },
+
   render: function () {
+    var heightPerNode = 112;
     var mangaNodes = this.props.data.filter(function (manga) {
       return manga.title.toLowerCase().indexOf(this.props.keyword.toLowerCase()) >= 0;
     }.bind(this))
     .map(function (manga, idx) {
-      return <MangaNode key={idx} data={manga} load={this.props.load}/>
+      var position = idx * heightPerNode;
+      return <MangaNode hidden={position < this.state.top || position > this.state.bottom} data={manga} load={this.props.load} />
     }.bind(this));
 
     return (
-      <div className="listview">
-        <ul>
+      <div className="listview" onScroll={this._onScroll}>
+        <ul onScroll={this._onScroll}>
           {mangaNodes}
         </ul>
       </div>
     );
+  },
+
+  _onScroll: function(e) {
+    this.setState({
+      top: this.getDOMNode().scrollTop,
+      bottom: this.getDOMNode().scrollTop + this.getDOMNode().offsetHeight
+    })
   }
 });
 
