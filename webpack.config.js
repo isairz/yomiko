3cb3d1642ad450ff241b28f2e1bb603065cdec76
Seongjun Kim <isair.zun@gmail.com>
2015-07-12 21:04:56 +0900

    Remove css and use inline-style

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,8 @@ module.exports = {
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
-    new ExtractTextPlugin('app.css'),
     new HtmlWebpackPlugin({
-      title: 'Redux React Router Async Example',
+      title: 'Yomiko Develop Page',
       filename: 'index.html',
       template: 'index.template.html',
       favicon: path.join(__dirname, 'assets', 'images', 'favicon.png')
@@ -28,11 +27,7 @@ module.exports = {
   ],
   module: {
     loaders: [
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!cssnext-loader') },
       { test: /\.js$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ }
     ]
-  },
-  cssnext: {
-    browsers: 'last 2 versions'
   }
 };
