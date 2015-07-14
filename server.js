e5b97c22a33fae1f2862d7a89ec89293f4c85f7d
Seongjun Kim <isair.zun@gmail.com>
2015-07-14 11:42:25 +0900

    Add missing dependencies

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var compress = require('compression');
-var request = require('request');
 var config = require('./config');
 
 // all environments
@@ -9,14 +8,6 @@ app.set('port', config.PORT || 2643);
 app.use(compress());
 
 app.use('/assets', express.static('dist'));
-
-app.get('/image-proxy', function (req, res) {
-  request(req.query.src)
-  .on('error', function (err) {
-    res.sendStatus(404);
-  })
-  .pipe(res)
-});
 
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/dist/index.html')
