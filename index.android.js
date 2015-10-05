526ca6d220ca055cda6c5a3973a81e86fe2a8c34
Seongjun Kim <isair.zun@gmail.com>
2015-10-05 13:19:03 +0900

    Revert "Setup Webpack for using babel"
    React-native-webpack-server didn't support android.

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ var yomiko = React.createClass({
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to Yomiko Native!
+          Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,33 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var config = {
-  debug: true,
-  devtool: 'source-map',
-  entry: {
-    'index.ios': ['./src/main.ios.js'],
-    'index.android': ['./src/main.android.js'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].js',
-  },
-  module: {
-    loaders: [{
-      test: /\.(js|jsx|es6)$/,
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        stage: 0,
-        plugins: [],
-      }
-    }]
-  },
-  resolve: { extentions: ['', '.js', '.jsx', '.es6'] },
-  plugins: [
-    new webpack.NoErrorsPlugin(),
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:2643',
+    'webpack/hot/only-dev-server',
+    './lib/index'
   ],
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Yomiko Develop Page',
+      filename: 'index.html',
+      template: 'index.template.html',
+      favicon: path.join(__dirname, 'assets', 'images', 'favicon.png')
+    })
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
+    ]
+  }
 };
-
-// Hot loader
-if (process.env.HOT) {
-  console.log("How loader");
-  config.devtool = 'eval'; // Speed up incremental builds
-  config.entry['index.ios'].unshift('react-native-webpack-server/hot/entry');
-  config.entry['index.ios'].unshift('webpack/hot/only-dev-server');
-  config.entry['index.ios'].unshift('webpack-dev-server/client?http://localhost:8082');
-  config.output.publicPath = 'http://localhost:8082/';
-  config.plugins.unshift(new webpack.HotModuleReplacementPlugin());
-  config.module.loaders[0].query.plugins.push('react-transform');
-  config.module.loaders[0].query.extra = {
-    'react-transform': {
-      transforms: [{
-        transform: 'react-transform-hmr',
-        imports: ['react-native'],
-        locals: ['module']
-      }]
-    }
-  };
-}
-
-// Production config
-if (process.env.NODE_ENV === 'production') {
-  console.log("Production");
-  config.plugins.push(new webpack.optimize.OccurrenceOrderPlugin());
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
-}
-
-module.exports = config;
