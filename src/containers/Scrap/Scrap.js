5eead5c197f3e893ed4ef3081ca7789dce4dc7ca
Seongjun Kim <isair.zun@gmail.com>
2015-11-23 03:25:54 +0900

    Go chapter when chapter list has one.

--- src/containers/Scrap/Scrap.js
+++ src/containers/Scrap/Scrap.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import DocumentMeta from 'react-document-meta';
 import { connect } from 'react-redux';
 import { MangaList, MangaViewer, LoadingPage } from 'components';
+import { replaceState } from 'redux-router';
 import { isLoaded, load } from 'redux/modules/scrap';
 import connectData from 'helpers/connectData';
 
@@ -16,25 +17,31 @@ function fetchDataDeferred(getState, dispatch) {
 @connectData(null, fetchDataDeferred)
 @connect(
   state => ({
+    location: state.router.location,
     scrap: state.scrap.data,
     error: state.scrap.error,
     loading: state.scrap.loading
   }),
-  {})
+  {replaceState})
 export default
 class Scrap extends Component {
   static propTypes = {
     scrap: PropTypes.object,
     error: PropTypes.string,
     loading: PropTypes.bool,
+    location: PropTypes.object,
+    replaceState: PropTypes.func.isRequired,
   }
 
   render() {
-    const { scrap, error, loading } = this.props;
+    const { scrap, error, loading, location } = this.props;
     const title = scrap && scrap.title || 'Project Yomiko';
     const styles = require('./Scrap.scss');
 
     let Child = null;
+    if (scrap.list && scrap.list.length === 1) {
+      this.props.replaceState(null, location.pathname, {link: scrap.list[0].link});
+    }
     switch (scrap && scrap.type) {
       case 'list':
         Child = (
