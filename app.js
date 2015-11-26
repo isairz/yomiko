4de78cb4b961f76d4bed1d531e8a89493fd357e6
Hyunje Alex Jun <me@noraesae.net>
2015-11-26 15:36:06 +0900

    Fix wrong indent in app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var compress = require('compression');
 var request = require('request').defaults({
   jar: true,
   headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36'
   }
 });
 var maru = require('./scraper/marumaru');
