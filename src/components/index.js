a2b9cfe7671a5db00d07af68c7f1d5692bd423b6
Seongjun Kim <isair.zun@gmail.com>
2015-12-01 19:01:54 +0900

    Remove unused component and styles

--- src/components/index.js
+++ src/components/index.js
@@ -6,12 +6,6 @@
  */
 
 export BackgroundImage from './BackgroundImage/BackgroundImage';
-export CounterButton from './CounterButton/CounterButton';
-export GithubButton from './GithubButton/GithubButton';
-export InfoBar from './InfoBar/InfoBar';
 export LoadingPage from './LoadingPage/LoadingPage';
 export MangaList from './MangaList/MangaList';
 export MangaViewer from './MangaViewer/MangaViewer';
-export MiniInfoBar from './MiniInfoBar/MiniInfoBar';
-export SurveyForm from './SurveyForm/SurveyForm';
-export WidgetForm from './WidgetForm/WidgetForm';
