bc1792aa84d5dc6057e8e49b789a69e76a037fc7
Seongjun Kim <isair.zun@gmail.com>
2016-10-22 00:18:05 +0900

    Boooooom for Rewrite

--- api/actions/index.js
+++
@@ -1,8 +0,0 @@
-export loadInfo from './loadInfo';
-export loadAuth from './loadAuth';
-export login from './login';
-export logout from './logout';
-export * as widget from './widget/index';
-export scrap from './scrap';
-export imageProxy from './imageProxy';
-export download from './download';
--- src/containers/index.js
+++
@@ -1,7 +0,0 @@
-export App from './App/App';
-export Chat from './Chat/Chat';
-export Home from './Home/Home';
-export Scrap from './Scrap/Scrap';
-export Login from './Login/Login';
-export LoginSuccess from './LoginSuccess/LoginSuccess';
-export NotFound from './NotFound/NotFound';
