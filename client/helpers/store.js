cce58d44459f162e09eb64bd7437e114b8964133
Seongjun Kim <isair.zun@gmail.com>
2016-10-23 05:39:00 +0900

    Load Data From db

--- client/helpers/store.js
+++ client/helpers/store.js
@@ -3,10 +3,61 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+import callApi from './callApi'
+
+function makeParams(param, value) {
+  switch (param) {
+    case 'id':
+    case 'name':
+    case 'type':
+    case 'language':
+      return { [param]: `eq.${value}` }
+    case 'author':
+    case 'group':
+    case 'character':
+    case 'tag':
+      return  { [param + 's']: `@>.{${value}}` }
+    case 'series':
+      return  { [param + 'es']: `@>.{${value}}` }
+  }
+}
+
 const store = new Vuex.Store({
-  state: {},
-  actions: {},
-  mutations: {},
+  state: {
+    manga: {
+      list: [],
+      info: {},
+      itemsPerPage: 12,
+    },
+  },
+  actions: {
+    // ensure data for rendering given list type
+    FETCH_MANGA_LIST: ({ commit, state }, { param, value, page }) =>
+      callApi('mangas', makeParams(param, value))
+      .page(page, state.manga.itemsPerPage)
+      .then(json => commit('SET_MANGA_LIST', json)),
+
+    FETCH_MANGA: ({ commit }, id) =>
+      callApi(`mangas?id=eq.${id}`)
+      .then(json => commit('SET_MANGA', json)),
+  },
+  mutations: {
+    SET_MANGA_LIST: (state, list) => {
+      state.manga.list = list
+      list.forEach(manga => {
+        if (!state.manga.info[manga.id]) state.manga.info[manga.id] = manga
+      })
+    },
+
+    SET_MANGA_PAGE: (state, { id, pages }) => {
+      state.manga.info[id].pages = pages
+    },
+  },
+
+  getters: {
+    mangaList: state => state.manga.list,
+    mangaInfo: state => id => state.manga.info[id],
+  },
 })
 
 export default store
--- client/views/MangaListView.vue
+++ client/views/MangaListView.vue
@@ -20,32 +20,51 @@ export default {
   components: {
     MangaList,
   },
-
+  data () {
+    return {
+      transition: 'slide-left',
+      items: [],
+    }
+  },
   computed: {
     page () {
-      return 5
       return Number(this.$store.state.route.query.page) || 1
     },
     maxPage () {
-      return 20
-      const { itemsPerPage, list } = this.$store.state.manga
-      return Math.ceil(list.length / itemsPerPage)
+      return this.items.fullLength ? Math.ceil(this.items.fullLength / this.$store.state.manga.itemsPerPage) : 1
     },
     pagination () {
-      const from = Math.max(this.page - 5, 1)
-      const to = Math.min(this.page + 5, this.maxPage)
+      const from = Math.max(this.page - 3, 1)
+      const to = Math.min(this.page + 3, this.maxPage)
+      if (!this.items.fullLength) return []
       return Array.from(Array(to - from + 1), (_, i) => (from + i))
     },
     hasMore () {
       return this.page < this.maxPage
     },
   },
-  data () {
-    return {
-      /* eslint-disalbe */
-      items: [{"id":980555,"name":"Made fur 3","authors":["ransyou equals zero"],"groups":["096"],"type":"doujinshi","language":"japanese","serieses":null,"characters":null,"tags":["male:furry","male:males only","male:yaoi"],"thumbnail":"1.jpg","createdAt":"2016-09-27T22:39:20.136+09:00","updatedAt":"2016-09-29T21:51:09.721+09:00"},{"id":980541,"name":"Step Child Swapping","authors":["satsuki imonet"],"groups":null,"type":"manga","language":"russian","serieses":null,"characters":null,"tags":["female:big breasts","female:daughter","female:loli","female:milf","female:mother","incest","inseki","male:shota","mosaic censorship"],"thumbnail":"1.png","createdAt":"2016-09-27T22:39:20.15+09:00","updatedAt":"2016-09-29T21:51:09.732+09:00"},{"id":980535,"name":"Hitozuma Daisuki! Hiroyuki-kun","authors":["chin"],"groups":null,"type":"manga","language":"russian","serieses":null,"characters":null,"tags":["female:apron","female:big breasts","female:milf","female:nakadashi","male:bbm"],"thumbnail":"1.png","createdAt":"2016-09-27T22:39:20.167+09:00","updatedAt":"2016-09-29T21:51:09.741+09:00"},{"id":980343,"name":"Mei wa 〇nensei JS4 Hen","authors":["suzuki amaharu"],"groups":["jibaku-system"],"type":"doujinshi","language":"japanese","serieses":null,"characters":null,"tags":["female:anal","female:daughter","female:loli","female:niece","female:randoseru","female:scat","female:sole female","female:stockings","incest","male:sole male","mosaic censorship"],"thumbnail":"1.jpg","createdAt":"2016-09-27T22:39:20.187+09:00","updatedAt":"2016-09-29T21:51:09.771+09:00"},{"id":980528,"name":"Wakuwaku One-sans ch. 1-5","authors":["john k. pe-ta"],"groups":null,"type":"manga","language":"russian","serieses":null,"characters":null,"tags":["female:big breasts","female:nakadashi","female:x-ray","uncensored"],"thumbnail":"001_01.png","createdAt":"2016-09-27T22:39:20.215+09:00","updatedAt":"2016-09-29T21:51:09.779+09:00"},{"id":980516,"name":"After Festival","authors":["okara"],"groups":["aspergillus"],"type":"doujinshi","language":"russian","serieses":["toaru kagaku no railgun"],"characters":["mikoto misaka","touma kamijou"],"tags":["female:sole female","male:sole male"],"thumbnail":"1.png","createdAt":"2016-09-27T22:39:20.312+09:00","updatedAt":"2016-09-29T21:51:09.82+09:00"},{"id":980464,"name":"Classmate Nyuugaku Shashin 2 Rijichou Sensei to Issho ni Satsuei Kai","authors":["ohigetan"],"groups":["circle ohigetan"],"type":"doujinshi","language":"english","serieses":null,"characters":null,"tags":["female:big breasts","female:blowjob","female:bukkake","female:lactation","female:paizuri","full color","group"],"thumbnail":"1.jpg","createdAt":"2016-09-27T22:39:20.397+09:00","updatedAt":"2016-09-29T21:51:09.877+09:00"},{"id":980493,"name":"ANOTHER ONE","authors":["yanagi hirohiko"],"groups":["akkan-bi project"],"type":"doujinshi","language":"spanish","serieses":["dead or alive"],"characters":["kasumi"],"tags":["female:big breasts","female:bondage","female:cervix penetration","female:x-ray"],"thumbnail":"1.png","createdAt":"2016-09-27T22:39:20.343+09:00","updatedAt":"2016-09-29T21:51:09.84+09:00"},{"id":980474,"name":"Si-Eun Ch.0-34","authors":null,"groups":null,"type":"doujinshi","language":"korean","serieses":null,"characters":null,"tags":["full color","story arc","webtoon"],"thumbnail":"001_Si_Eun.jpg","createdAt":"2016-09-27T22:39:20.353+09:00","updatedAt":"2016-09-29T21:51:09.849+09:00"},{"id":980422,"name":"Oniyuri no Hanakotoba","authors":["nekomata naomi"],"groups":["nekomataya"],"type":"doujinshi","language":"french","serieses":["kantai collection"],"characters":["tatsuta","tenryuu"],"tags":["female:big breasts","female:blowjob","female:eyepatch","female:futanari","female:sole dickgirl","female:sole female","female:stockings","female:x-ray"],"thumbnail":"1.jpg","createdAt":"2016-09-27T22:39:20.419+09:00","updatedAt":"2016-09-29T21:51:09.892+09:00"},{"id":978585,"name":"Tamaya Roppatsume","authors":["yukino minato"],"groups":["noraneko-no-tama"],"type":"doujinshi","language":"japanese","serieses":null,"characters":null,"tags":["female:loli"],"thumbnail":"1.jpg","createdAt":"2016-09-27T22:39:40.955+09:00","updatedAt":"2016-09-29T21:50:25.615+09:00"},{"id":978374,"name":"Hari's Lumpy Sharehouse Ch.0-10","authors":null,"groups":null,"type":"doujinshi","language":"korean","serieses":null,"characters":null,"tags":["full color","story arc","webtoon"],"thumbnail":"001_Haris_lumpy_sharehouse.jpg","createdAt":"2016-09-27T22:39:45.1+09:00","updatedAt":"2016-09-29T21:50:25.78+09:00"}]
-      /* eslint-enable */
-    }
+  watch: {
+    page (to, from) {
+      this.loadItems(to, from)
+    },
+  },
+  beforeMount () {
+    this.loadItems(this.page)
+  },
+  methods: {
+    loadItems (to = this.page, from = -1) {
+      this.loading = true
+      this.$store.dispatch('FETCH_MANGA_LIST', {
+        page: this.page,
+      }).then(() => {
+        if (this.page < 0 || this.page > this.maxPage) {
+          return
+        }
+        this.transition = from === -1 ? '' : to > from ? 'slide-left' : 'slide-right'
+        this.items = this.$store.getters.mangaList
+        // this.loading = false
+      })
+    },
   },
 }
 </script>
