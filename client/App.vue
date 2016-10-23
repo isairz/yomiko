e06fa8b3e7201d7bed508708c2da4e0530548fcf
Seongjun Kim <isair.zun@gmail.com>
2016-10-23 16:51:21 +0900

    Add bottom

--- client/App.vue
+++ client/App.vue
@@ -4,15 +4,19 @@
     <transition name="fade" mode="out-in">
       <router-view class="view"></router-view>
     </transition>
+    <bottom-info></bottom-info>
   </div>
 </template>
 
 <script>
 import TopNav from './components/TopNav.vue'
+import BottomInfo from './components/BottomInfo.vue'
+
 export default {
   name: 'App',
   components: {
     TopNav,
+    BottomInfo,
   },
 }
 </script>
---
+++ client/components/BottomInfo.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">
+        <p>
+          <p>&copy; 2016 &middot; Yomiko</p>
+        </p>
+        <p>
+          <p>We are on Twitter <a href='https://twitter.com/@prev_ious' target='_Blank'>@prev_ious</a></p>
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
